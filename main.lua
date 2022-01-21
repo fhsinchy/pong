bf05aa39051782830da57ce5637b7a50516957b2
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2022-01-21 23:26:01 +0600

    Low res implemented

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 -- invoked once
 function love.load()
-	love.window.setMode(
-		WINDOW_WIDTH,
-		WINDOW_HEIGHT,
+	love.window.setTitle('Pong')
+	
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,
 		{
 			fullscreen = false,
 			resizable = false,
@@ -14,13 +21,25 @@ function love.load()
 	)
 end
 
+-- invoked every key press
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
+
+
 -- invoked every frame
 function love.draw()
+	push:apply('start')
+
 	love.graphics.printf (
 		'Hello Pong!',
 		0,
-		WINDOW_HEIGHT / 2 - 6,
-		WINDOW_WIDTH,
+		VIRTUAL_HEIGHT / 2 - 6,
+		VIRTUAL_WIDTH,
 		'center'
 	)
+
+	push:apply('end')
 end

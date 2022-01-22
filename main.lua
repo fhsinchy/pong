2d2be98e4aaa209fd9802215c979b94eef606a27
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2022-01-23 00:37:42 +0600

    Implemented paddle interaction and scores

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 -- invoked once
 function love.load()
 	love.window.setTitle('Pong')
@@ -13,6 +15,7 @@ function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
 	smallFont = love.graphics.newFont('font.ttf', 8)
+	scoreFont = love.graphics.newFont('font.ttf', 32)
 
 	love.graphics.setFont(smallFont)
 
@@ -23,12 +26,36 @@ function love.load()
 			vsync = true
 		}
 	)
+
+	player1Score = 0
+	player2Score = 0
+
+	player1Y = 30
+	player2Y = VIRTUAL_HEIGHT - 50
 end
 
 -- invoked every key press
 function love.keypressed(key)
 	if key == 'escape' then
 		love.event.quit()
+	end
+end
+
+function love.update(dt)
+	if love.keyboard.isDown('w') then
+		player1Y = player1Y - PADDLE_SPEED * dt
+	end
+
+	if love.keyboard.isDown('s') then
+		player1Y = player1Y + PADDLE_SPEED * dt
+	end
+
+	if love.keyboard.isDown('up') then
+		player2Y = player2Y - PADDLE_SPEED * dt
+	end
+
+	if love.keyboard.isDown('down') then
+		player2Y = player2Y + PADDLE_SPEED * dt
 	end
 end
 
@@ -48,13 +75,17 @@ function love.draw()
 	)
 
 	-- left paddle
-	love.graphics.rectangle('fill', 10, 30, 5, 20)
+	love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 	
 	-- right paddle
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
 	-- ball
 	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2 , VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+	love.graphics.setFont(scoreFont)
+	love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+	love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
 	push:apply('end')
 end

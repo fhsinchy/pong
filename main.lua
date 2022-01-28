591ad8ff03559a8907ad2047d1256fbd3a63d796
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2022-01-28 22:05:12 +0600

    Added sounds

--- main.lua
+++ main.lua
@@ -30,6 +30,12 @@ function love.load()
 	scoreFont = love.graphics.newFont('font.ttf', 32)
 	love.graphics.setFont(smallFont)
 
+	sounds = {
+		['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+		['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+		['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+	}
+
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,
 		{
 			fullscreen = false,
@@ -68,12 +74,14 @@ function love.update(dt)
 		-- collision detection between the ball and player 1
 		if ball:collides(player1) then
 			ball.dx = -ball.dx * 1.03
+			sounds['paddle_hit']:play()
 			ball.x = player1.x + 5
 		end
 	
 		-- collision detection between the ball and player 2
 		if ball:collides(player2) then
 			ball.dx = -ball.dx * 1.03
+			sounds['paddle_hit']:play()
 			ball.x = player2.x - 5
 		end
 	
@@ -89,18 +97,21 @@ function love.update(dt)
 		if ball.y <= 0 then
 			ball.y = 0
 			ball.dy = -ball.dy
+			sounds['wall_hit']:play()
 		end
 	
 		-- -4 to account for the ball's size
 		if ball.y >= VIRTUAL_HEIGHT - 4 then
 			ball.y = VIRTUAL_HEIGHT - 4
 			ball.dy = -ball.dy
+			sounds['wall_hit']:play()
 		end
 
 		-- checks if the ball has passed through the left side of the screen
 		if ball.x < 0 then
 			servingPlayer = 1
 			player2Score = player2Score + 1
+			sounds['score']:play()
 
 			if player2Score == WINNING_SCORE then
 				gameState = 'done'
@@ -115,6 +126,7 @@ function love.update(dt)
 		if ball.x > VIRTUAL_WIDTH then
 			servingPlayer = 2
 			player1Score = player1Score + 1
+			sounds['score']:play()
 			
 			if player1Score == WINNING_SCORE then
 				gameState = 'done'
